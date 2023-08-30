924c4f3c1412a8349debc5137b64df97b683ece2
Nawarian <nickolas@phpsp.org.br>
2023-08-30 15:02:29 -0300

    fix: quit on game over now works

--- src/main/java/br/com/codamos/InputHandler.java
+++ src/main/java/br/com/codamos/InputHandler.java
@@ -35,6 +35,7 @@ public class InputHandler implements KeyListener, MouseListener {
         }
 
         if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+            game.reset();
             game.stopGame();
         }
 
